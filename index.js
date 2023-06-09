841a88036703c1672ef2c9c566819f01b40a1589
gaurav10000 <gauravsaraswatofficial@gmail.com>
2023-06-09 10:22:03 +0530

    installed node-fetch

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ app.use(express.json())
 app.use(express.static("public"))
 
 app.post("/cityinfo", async(req, res)=>{
-    // const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=1394c8f8681a496cb5c121423232705&q=${req.body.city}`)
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=1394c8f8681a496cb5c121423232705&q=${req.body.city}`)
 
     
     const data = await response.json()
     console.log(data)
     res.render("weather.ejs",{name: data.location.name, temp_cel: data.current.temp_c, weather_cond: data.current.condition.text, icon: data.current.condition.icon})
-    // res.send("no data")
 })
 
 app.get("/", (req, res)=>{
