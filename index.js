e0d2814018e9296c3ae0c60b6f0d3f00f41a9732
gaurav10000 <gauravsaraswatofficial@gmail.com>
2023-06-09 13:44:28 +0530

    some useful new features

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-import express from "express"
-import ejs from "ejs"
+import express from "express";
+import ejs from "ejs";
 import cookieParser from "cookie-parser";
 import fetch from "node-fetch";
 const app = express();
 
 // USING MIDDLEWARES
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static("public"));
 
-app.post("/cityinfo", async(req, res)=>{
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=1394c8f8681a496cb5c121423232705&q=${req.body.city}`)
+app.post("/cityinfo", async (req, res) => {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=1394c8f8681a496cb5c121423232705&q=${req.body.city}`
+  );
 
-    
-    const data = await response.json()
-    console.log(data)
-    res.render("weather.ejs",{name: data.location.name, temp_cel: data.current.temp_c, weather_cond: data.current.condition.text, icon: data.current.condition.icon})
-})
+  const data = await response.json();
+  console.log(data);
 
-app.get("/", (req, res)=>{
-    res.render("Home.ejs");
-})
+      
 
-app.listen(3000, (req, res)=>{
-    console.log("Application has been started on port 3000");
-})
+  try{
+    let cityName =  data.location.name
+      let cityTemp_cel =  data.current.temp_c
+      let cityWeather_cond =  data.current.condition.text
+      let waetherIcon =  data.current.condition.icon
+
+    res.render("weather.ejs", {
+      name: cityName,
+      temp_cel: cityTemp_cel,
+      weather_cond: cityWeather_cond,
+      icon: waetherIcon,
+    });
+  } catch (error) {
+    console.log(data.error.message);
+    let message = data.error.message;
+    res.render("error.ejs", { msg: message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("Home.ejs");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Application has been started on port 3000");
+});
